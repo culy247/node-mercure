f49d812c4b22626c1530894bea30c36a84ae01c8
MeoChuot <diep.pham@amela.vn>
2021-03-13 02:05:16 +0700

    try again

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Server } = require( 'mercure' );
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt( process.env.PORT || 5000 );
 const server = new Server( {
     jwtKey: '!UnsecureChangeMe!',
     path: '/.well-known/mercure',
