253faaf48043ec177b2dee9bc7ab46ae0ad7260d
MeoChuot <diep.pham@amela.vn>
2021-03-13 01:51:22 +0700

    update port

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const { Server } = require( 'mercure' );
-const config = require( "platformsh-config" ).config();
-const credentials = config.credentials( 'redis' );
-console.log( config, credentials );
+const PORT = process.env.PORT || 5000;
 const server = new Server( {
     jwtKey: '!UnsecureChangeMe!',
     path: '/.well-known/mercure',
-    redis: {
-        port: credentials.port,
-        host: credentials.host
-    }
 } );
 
-server.listen( config.environmentVariables.PORT );
+server.listen( PORT );
