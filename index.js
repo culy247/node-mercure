9ff3b44401965b441b2b73740cfcdf72cc4f1d2f
MeoChuot <diep.pham@amela.vn>
2021-03-14 12:27:33 +0700

    fix uuid v4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const express = require( 'express' );
 const { Server } = require( './src' );
 const cors = require( 'cors' );
 
@@ -6,13 +7,13 @@ class App extends Server
     configure ()
     {
         this.app.use( cors() );
-        this.app.use( '/', function ( req, res ) { res.send( 'hellop' ); } );
+        this.app.use( express.static( 'public' ) );
     }
 }
 
 const server = new App( {
     jwtKey: process.env.JWT_KEY || '!ChangeMe!',
-    path: '/hub',
+    path: process.env.HUB || '/hub',
     redis: {
         url: process.env.REDIS_DSN || ''
     }
